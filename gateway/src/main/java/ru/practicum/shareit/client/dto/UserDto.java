4fc8994eece56d101f43a31816787bbb0588af60
andrej1307 <andrej.stelmaschuk@yandex.ru>
2025-04-24 16:20:45 +0700

    feat:  добавлен пакет user в модуль gateway

---
+++ gateway/src/main/java/ru/practicum/shareit/client/dto/UserDto.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.client.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ru.practicum.shareit.validator.ValidAction;
+
+@Data
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+
+    @NotBlank(message = "Имя не может быть пустым", groups = {ValidAction.OnCreate.class})
+    private String name;
+
+    @NotBlank(message = "Email не может быть пустым", groups = {ValidAction.OnCreate.class})
+    @Email(message = "Email должен удовлетворять правилам формирования почтовых адресов.",
+            groups = {ValidAction.OnCreate.class, ValidAction.OnUpdate.class})
+    private String email;
+}

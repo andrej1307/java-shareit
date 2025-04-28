70e4e8f403ff89cd25b34ab7433467a09c4ab79a
Andrej Stelmaschuk <andrej.stelmaschuk@yandex.ru>
2025-04-28 22:38:05 +0700

    feat: пишем тесты ItemControllerTest 2

--- server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -2,11 +2,13 @@ package ru.practicum.shareit.item;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemCommentsDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.CommentService;
@@ -14,6 +16,7 @@ import ru.practicum.shareit.item.service.ItemService;
 import ru.practicum.shareit.user.UserController;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,10 +35,8 @@ class ItemControllerTest {
 
     @Autowired
     ObjectMapper mapper;
-
     @MockBean
     private ItemService itemService;
-
     @MockBean
     private CommentService commentService;
 
@@ -93,7 +93,7 @@ class ItemControllerTest {
     void createItem() throws Exception {
         ItemDto itemDto = new ItemDto();
         itemDto.setId(1L);
-        itemDto.setName("Name item");
+        itemDto.setName("Name item3");
         itemDto.setDescription("controller test create new item");
         itemDto.setAvailable(true);
 
@@ -112,5 +112,72 @@ class ItemControllerTest {
                 .andExpect(jsonPath("$.description", is(itemDto.getDescription())));
     }
 
+@Test
+    void updateItem() throws Exception {
+    ItemDto itemDto = new ItemDto();
+    itemDto.setId(1L);
+    itemDto.setName("Name item4");
+    itemDto.setDescription("controller test update item");
+    itemDto.setAvailable(true);
 
+    when(itemService.updateItem(any(), anyLong()))
+            .thenReturn(itemDto);
+    mvc.perform(patch("/items/1")
+                    .header(HEADER_USER_ID, 1L)
+                    .content(mapper.writeValueAsString(itemDto))
+                    .characterEncoding(StandardCharsets.UTF_8)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id", is(itemDto.getId()), Long.class))
+            .andExpect(jsonPath("$.name", is(itemDto.getName())))
+            .andExpect(jsonPath("$.description", is(itemDto.getDescription())));
+    }
+
+    @Test
+    void deleteItem() throws Exception {
+        mvc.perform(delete("/items/1")
+                        .header(HEADER_USER_ID, 1L)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        Mockito.verify(itemService, Mockito.times(1))
+                .deleteItem(1L, 1L);
+    }
+
+    @Test
+    void deleteAllItems() throws Exception {
+        mvc.perform(delete("/items")
+                        .header(HEADER_USER_ID, 1L)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        Mockito.verify(itemService, Mockito.times(1))
+                .deleteAllItems();
+    }
+
+    @Test
+    void addComment() throws Exception {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(1L);
+        commentDto.setItemId(1L);
+        commentDto.setText("comment item");
+        commentDto.setAuthorId(2L);
+        commentDto.setCreated(Instant.now());
+
+        when(commentService.addComment(any()))
+                .thenReturn(commentDto);
+
+        mvc.perform(post("/items/1/comment")
+                        .header(HEADER_USER_ID, 2L)
+                        .content(mapper.writeValueAsString(commentDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
 }

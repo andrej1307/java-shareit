dc822159400d1dcf1fbdd854929f885a58a74f7a
Andrej Stelmaschuk <andrej.stelmaschuk@yandex.ru>
2025-05-03 22:52:30 +0700

    fix: исправление ошибок стиля

--- server/src/test/java/ru/practicum/shareit/item/model/ItemTest.java
+++ server/src/test/java/ru/practicum/shareit/item/model/ItemTest.java
@@ -6,11 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
-import ru.practicum.shareit.item.dto.CommentDto;
-import ru.practicum.shareit.user.User;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @JsonTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)

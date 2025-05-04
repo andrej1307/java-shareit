f3dcb3730c414081f3e71cdfe1392cb65a8d8bce
Andrej Stelmaschuk <andrej.stelmaschuk@yandex.ru>
2025-05-04 17:12:13 +0700

    fix: увеличиваем покрытие тестами 2

--- server/src/main/java/ru/practicum/shareit/advisor/ErrorAdvisor.java
+++ server/src/main/java/ru/practicum/shareit/advisor/ErrorAdvisor.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.advisor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,41 +10,12 @@ import ru.practicum.shareit.excepton.InternalServerException;
 import ru.practicum.shareit.excepton.NotFoundException;
 import ru.practicum.shareit.excepton.ValidationException;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
  * Класс обработки исключений при обработке поступивших http запросов
  */
 @Slf4j
 @RestControllerAdvice
 public class ErrorAdvisor {
-
-    /**
-     * Обработка исключения MethodArgumentNotValidException - при проверке аргумента метода
-     *
-     * @param e - исключение
-     * @return - список нарушений для отображения в теле ответа
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorMessage onMethodArgumentNotValidException(
-            MethodArgumentNotValidException e
-    ) {
-        final List<ErrorMessage> violations = e.getBindingResult().getFieldErrors().stream()
-                .map(error -> new ErrorMessage("[" + error.getField() + "] "
-                        + error.getDefaultMessage()))
-                .collect(Collectors.toList());
-        log.info("400 {}.", e.getMessage());
-        return violations.get(0);
-    }
-
-    /**
-     * Метод обработки пользовательского исключения ValidationException
-     *
-     * @param exception - исключение проверки данных
-     * @return - объект для http ответа с сообщением об ошибке
-     */
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorMessage onValidationException(ValidationException exception) {
@@ -59,20 +28,6 @@ public class ErrorAdvisor {
     public ErrorMessage notFoundObject(NotFoundException exception) {
         log.info("404 {}.", exception.getMessage());
         return new ErrorMessage(exception.getMessage());
-    }
-
-    /**
-     * Обработка исключения HttpMessageNotReadableException при поступлении пустого запроса
-     *
-     * @param e - исключение генерируемое при отсутствии обязательных данных в теле запроса
-     * @return - объект для http ответа с сообщением об ошибке
-     */
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorMessage onHttpMessageNotReadableException(
-            HttpMessageNotReadableException e) {
-        log.info("400 {}.", e.getMessage());
-        return new ErrorMessage("В запросе отсутствуют необходимые данные." + e.getMessage());
     }
 
     @ExceptionHandler
@@ -89,12 +44,6 @@ public class ErrorAdvisor {
         return new ErrorMessage(e.getMessage());
     }
 
-    /**
-     * Обработка непредвиденного исключения
-     *
-     * @param e - исключение
-     * @return - сообщение об ошибке
-     */
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage handleException(final Exception e) {
